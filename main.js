0a368fd86aebf67fe687d213fb28a542292e6151
Mattheo de Wit <mattheodw@gmail.com>
2020-09-01 13:50:31 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var courses = ["info", "mentorate", "OOP", "RL", "CO"]
+var courses = ["info", "meme", "mentorate", "OOP", "RL", "CO"]
 function view(a){
 let x = courses.indexOf(a)
 for (var i = 0; i<courses.length; i++){
@@ -13,9 +13,33 @@ for (var i = 0; i<courses.length; i++){
     document.getElementById(a).style.visibility = "hidden"
     let y = courses[x] + "button"
     document.getElementById(y).style.background = "#eeeeee"
+
   } else {
   	let y = courses[x] + "button"
     document.getElementById(a).style.visibility = "visible"
     document.getElementById(y).style.background = "lightblue"
+        if (a == "meme"){
+    document.getElementById("meme").innerHTML = ""
+   	newmeme();
+    }
   }
+}
+
+
+function newmeme(){
+    let requestURL = 'https://meme-api.herokuapp.com/gimme';
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'text';
+    request.send();
+    request.onload = function() {
+      let x = request.response;
+      let y = JSON.parse(x);
+      var img = document.createElement("img");
+      img.src = y['url']
+      img.width = 400
+      img.height = 400
+      var src = document.getElementById("meme")
+      src.appendChild(img)
+    }
 }
